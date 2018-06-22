7bd6387af6be0889ce25b5d2114bdf399251182a
shullmb <shullmb@gmail.com>
2018-06-22 14:39:36 -0700

    added 404 test to index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -6,4 +6,8 @@ describe('App', function() {
   it('should return a 200 response', function(done) {
     request(app).get('/').expect(200, done);
   });
+
+  it('should return a 404 response for a bad route', function(done){
+    request(app).get('/asdads').expect(404, done);
+  })
 });
